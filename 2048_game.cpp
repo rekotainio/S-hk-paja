81a9151a265ec711fd3381ebe19e94472fc8f502
rekotainio <105646808+rekotainio@users.noreply.github.com>
2024-05-21 10:01:42 +0300

    Update 2048_game.cpp
    Uusi väripaletti

--- 2048_game.cpp
+++ 2048_game.cpp
@@ -159,37 +159,37 @@ void flattenBoard(){
 void vectorToColors(){
   for (int i = 0; i < LED_COUNT; ++i) {
     if(newBoard[i] == 2){
-      colors[LED_COUNT - 1 - i] = rgb_color(221,255,0);
+      colors[LED_COUNT - 1 - i] = rgb_color(230,25,75);
     }
     if(newBoard[i] == 4){
-      colors[LED_COUNT - 1 - i] = rgb_color(82,255,0);
+      colors[LED_COUNT - 1 - i] = rgb_color(245,130,48);
     }
     if(newBoard[i] == 8){
-      colors[LED_COUNT - 1 - i] = rgb_color(0,255,57);
+      colors[LED_COUNT - 1 - i] = rgb_color(255,225,25);
     }
     if(newBoard[i] == 16){
-      colors[LED_COUNT - 1 - i] = rgb_color(0,255,196);
+      colors[LED_COUNT - 1 - i] = rgb_color(210,245,60);
     }
     if(newBoard[i] == 32){
-      colors[LED_COUNT - 1 - i] = rgb_color(0,175,255);
+      colors[LED_COUNT - 1 - i] = rgb_color(60,180,75);
     }
     if(newBoard[i] == 64){
-      colors[LED_COUNT - 1 - i] = rgb_color(0,36,255);
+      colors[LED_COUNT - 1 - i] = rgb_color(70,240,240);
     }
         if(newBoard[i] == 128){
-      colors[LED_COUNT - 1 - i] = rgb_color(104,0,255);
+      colors[LED_COUNT - 1 - i] = rgb_color(0,130,200);
     }
     if(newBoard[i] == 256){
-      colors[LED_COUNT - 1 - i] = rgb_color(243,0,255);
+      colors[LED_COUNT - 1 - i] = rgb_color(145,30,180);
     }
     if(newBoard[i] == 512){
-      colors[LED_COUNT - 1 - i] = rgb_color(255,0,128);
+      colors[LED_COUNT - 1 - i] = rgb_color(240,50,230);
     }
     if(newBoard[i] == 1024){
-      colors[LED_COUNT - 1 - i] = rgb_color(255,11,0);
+      colors[LED_COUNT - 1 - i] = rgb_color(128,128,128);
     }
     if(newBoard[i] == 2048){
-      colors[LED_COUNT - 1 - i] = rgb_color(255,150,0);
+      colors[LED_COUNT - 1 - i] = rgb_color(255,255,255);
     }
 
     if(newBoard[i] == 0){
